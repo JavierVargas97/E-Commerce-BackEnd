0761f4bb716cb1d23d1e1c20c31aad1b9271eb03
javiervargas <javiervg1802@gmail.com>
2022-08-20 01:15:18 -0500

    Columns defined

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
-
-class Category extends Model {}
-
-Category.init(
-  {
-    // define columns
+class Category extends Model { }
+Category.init({
+  //Define Columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
-);
-
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'category',
+});
 module.exports = Category;
